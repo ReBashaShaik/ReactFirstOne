fab13812dc22e48c29ac5bafceab729bf58593eb
ReBashaShaik <janibashashaik2001@gmail.com>
2024-01-11 09:29:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App(){
+    return <div>
+               <h1>Welcome PacketPrep</h1>
+               <h2>Today we are going to learn ReactJS</h2>
+               <h1>React JS</h1>
+               <img src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" height="130px" width="150px"/>
+            
+               <p>ReactJs is a javaScript library which is used to build the User Interfaces 
+                and its developed by <mark>Facebook</mark>, Now a days most of companies using 
+                ReactJs to build their websites 
+               </p>
+
+        <h1>{new Date().toLocaleTimeString()}</h1>
+           </div> 
+
+}
+export default App; // here i am exporting this App.js file 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+setInterval(()=>{root.render(<App />);},1000);
